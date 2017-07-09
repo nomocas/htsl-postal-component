27c67c42f61ac1a72f5a62de1d15bff6f969b9c6
Gilles Coomans <gilles.coomans@gmail.com>
2017-07-09 23:47:41 +0200

    fix(name): rename string-pragmas to string-pragmatics

--- src/string-pragmas.js
+++
@@ -1,9 +0,0 @@
-/*
-* @Author: Gilles Coomans
-* @Date:   2017-04-11 23:38:46
-* @Last Modified by:   Gilles Coomans
-* @Last Modified time: 2017-04-11 23:41:41
-*/
-
-import htmlLexicon from 'babelute-html-lexicon';
-htmlLexicon.addAtoms(['postalComponent']);
---
+++ src/string-pragmatics.js
@@ -0,0 +1,9 @@
+/*
+* @Author: Gilles Coomans
+* @Date:   2017-04-11 23:38:46
+* @Last Modified by:   gilles
+* @Last Modified time: 2017-04-22 19:22:56
+*/
+
+import htmlLexicon from 'htsl-lexicon';
+htmlLexicon.addAtoms(['postalComponent']);
